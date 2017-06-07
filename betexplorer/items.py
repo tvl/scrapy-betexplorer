1915613760d93ad3a63b3f0252c49396430e63bd
Vladimir Tarasov <vt@unistra.ru>
2017-06-07 16:42:02 +0300

    Add matches spider

---
+++ betexplorer/items.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Item, Field
+
+class BetexplorerItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class Match(Item):
+    id = Field()
+    datetime = Field()
+    #area_id = Field()
+    area_name = Field()
+    #competition_id = Field()
+    competition_name = Field()
+    #home_team_id = Field()
+    home_team = Field()
+    #away_team_id = Field()
+    away_team = Field()
+    kick_off = Field()
+    score = Field()
+    fts = Field()
+    odd1 = Field()
+    oddX = Field()
+    odd2 = Field()
+    updated = Field()
+
+
---
+++ betexplorer/spiders/matches.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider, Request
+from scrapy.loader import ItemLoader
+from betexplorer.items import Match
+from urllib.parse import urlencode, urlparse, parse_qs
+from datetime import datetime, timezone
+
+class MatchesSpider(Spider):
+    name = "matches"
+    allowed_domains = ["http://www.betexplorer.com"]
+    start_urls = ['http://www.betexplorer.com/next/soccer/']
+    params = {
+        "sport": "soccer",
+        "page": "match",
+        "id" : "2255155",
+        "localization_id": "www"
+    }
+    def start_requests(self):
+        start_url = 'http://www.betexplorer.com/next/soccer/'
+        request = Request(url=start_url, callback=self.parse)
+        #request.meta['proxy'] = 'http://127.0.0.1:8118'
+        yield request
+
+    def parse(self, response):
+        items = []
+        competitions = response.xpath('//table[@class="table-matches js-nrbanner-t"]//tbody')
+        for c in competitions:
+            item = Match()
+            item['area_name'], item['competition_name'] = \
+                    c.xpath('./tr[@class="js-tournament"]//a/text()').extract_first().split(':')
+            matches = c.xpath('.//tr[@data-def="0"]')
+            for m in matches:
+                item['id'] = 0
+                dd, mm, yy, hh, mmm = list(map(int, m.xpath('.//@data-dt').extract_first().split(',')))
+                item['datetime'] = datetime(yy, mm, dd, hh, mmm, 0).isoformat(' ')
+                item['kick_off'], item['home_team'], item['away_team'] = \
+                        m.xpath('.//td[@class="table-matches__tt"]//span//text()').extract()
+                item['score'] = m.xpath('.//td[@class="table-matches__result"]//text()').extract_first()
+                item['fts'] = m.xpath('.//td[@class="table-matches__partial"]//text()').extract_first()
+
+                odds1x2 =  m.xpath('.//td[@class="table-matches__odds"]//a//@data-odd').extract()
+                if odds1x2:
+                    item['odd1'], item['oddX'],item['odd2'] = odds1x2
+            item['updated'] = datetime.utcnow().isoformat(' ')
+            yield item
+            items.append(item)
+        return items
+        #self.log('URL: {}'.format(response.url))
+
+    """
+    def parse(self, response):
+        venue = Venue()
+        venue['country'], venue['city'], venue['name'] = response.css('title::text')[0].extract().split(',')
+        res = response.xpath('//td//b/text()')
+        if len(res) > 0:
+            venue['opened'] = res[0].extract()
+        res = response.xpath('//td//b/text()')
+        if len(res) > 1:
+            venue['capacity'] = res[1].extract()
+        venue['lat'], venue['lng'] = response.xpath('//script/text()')[1].re(r'\((.*)\)')[1].split(',')
+        return venue
+    """
+
